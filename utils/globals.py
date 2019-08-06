4d8c6ac53e5c893a1d66722537cb1e2bf035ae15
balint <kincsesbalint@gmail.com>
2019-08-06 13:09:49 +0200

    globals

---
+++ utils/globals.py
@@ -0,0 +1,9 @@
+import os
+
+_SinkDir_ = "."
+_FSLDIR_ = os.environ['FSLDIR']
+
+#reference resolution could be changed here
+_brainref="/data/standard/MNI152_T1_1mm_brain.nii.gz"
+_headref="/data/standard/MNI152_T1_1mm.nii.gz"
+_brainref_mask="/data/standard/MNI152_T1_1mm_brain_mask_dil.nii.gz"
